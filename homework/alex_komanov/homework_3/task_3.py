20d7f37e02298a533aa9d7e6e0999e76475b8226
AlexKomanov <akomanov88@gmail.com>
2024-12-14 16:36:11 +0200

    adding homework_3 files

---
+++ homework/alex_komanov/homework_3/task_3.py
@@ -0,0 +1,8 @@
+a = 20
+b = 5
+
+arithmetic_mean = (a + b) / 2
+geometric_mean = (a * b) ** 0.5
+
+print(f"Среднее арифметическое: {arithmetic_mean}")
+print(f"Среднее геометрическое: {geometric_mean}")
---
+++ homework/alex_komanov/homework_3/task_4.py
@@ -0,0 +1,8 @@
+a = 3
+b = 4
+
+hypotenuse = (a**2 + b**2) ** 0.5
+area = 0.5 * a * b
+
+print(f"Гипотенуза: {hypotenuse}")
+print(f"Площадь: {area}")
