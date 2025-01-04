96b9b8dc444931edc05081214c214aa6647e7b51
AlexKomanov <akomanov88@gmail.com>
2025-01-04 13:02:19 +0200

    fixing linter failures

--- homework/alex_komanov/homework_5/task_3.py
+++ homework/alex_komanov/homework_5/task_3.py
@@ -3,4 +3,4 @@ subjects = ['math', 'biology', 'geography']
 
 # Students Ivanov, Petrov, Sidorov study these subjects: math, biology, geography
 
-print(f"Students {", ".join(students)} study these subjects: {", ".join(subjects)}")
+print(f"Students {', '.join(students)} study these subjects: {', '.join(subjects)}")
